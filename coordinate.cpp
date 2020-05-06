af93025c725b7f5a4db5c114de42656bebe94d6b
Marek Fiala <mafialka@gmail.com>
2020-05-07 00:12:50 +0200

    coord done

--- coordinate.cpp
+++ coordinate.cpp
@@ -5,20 +5,13 @@ Coordinate::Coordinate(QWidget *parent) : QWidget(parent)
 
 }*/
 
-Coordinate::Coordinate(double x, double y){
-    this->x = x;
-    this->y = y;
-}
-
-<<<<<<< HEAD
 Coordinate::Coordinate(double x, double y)
 {
     this->x = x;
     this->y = y;
 }
-=======
+
 Coordinate::Coordinate(){}
->>>>>>> e29ee25652fe3aa1874effeea5303d9c405df110
 
 void Coordinate::setX(double x)
 {
